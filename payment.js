2f932efd76015a3d75d3a7e37f1ff7e0f38fdf51
abhishekpundir789 <apundir@algomau.ca>
2020-10-04 23:22:45 -0700

    final commit

--- payment.js
+++ payment.js
@@ -7,9 +7,10 @@ var province= prompt("Enter the province", "British Columbia");
 var accountBalance=prompt("Enter the balance", "0.0");
 var account_Balance= parseInt(accountBalance);
 var totalAccountBalance;
+calculate();
 // calculate the final balance after the penalty
 
-if(accountBalance>0 && accountBalance<1000){
+/*if(accountBalance>0 && accountBalance<1000){
     totalAccountBalance= account_Balance+(account_Balance*0.02);
     document.write("Full Name:"+ " "+ fullName);
     document.write("<br>");
@@ -51,4 +52,50 @@ else if(accountBalance==0){
     document.write("Total Owing:"+" "+0);
     document.write("<br>");
     document.write("Thank you for the Payment!!!");
+}*/
+
+// calculate by creating the functions
+
+function calculate(){
+    if(account_Balance>0 && account_Balance<1000){
+        totalAccountBalance= account_Balance+(account_Balance*0.02);
+        document.write("Full Name:"+ " "+ fullName);
+        document.write("<br>");
+        document.write("Address:"+" "+ address);
+        document.write("<br>");
+        document.write("City:"+" "+city);
+        document.write("<br>");
+        document.write("Province:"+" "+province);
+        document.write("<br>");
+        document.write("Account Balance:"+" "+accountBalance);
+        document.write("<br>");
+        document.write("Total Owing:"+" "+totalAccountBalance);
+    }
+    else if(account_Balance>=1000){
+        totalAccountBalance=account_Balance+(account_Balance*0.03);
+        document.write("Full Name:"+ " "+ fullName);
+        document.write("<br>");
+        document.write("Address:"+" "+ address);
+        document.write("<br>");
+        document.write("City:"+" "+city);
+        document.write("<br>");
+        document.write("Province:"+" "+province);
+        document.write("<br>");
+        document.write("Account Balance:"+" "+accountBalance);
+        document.write("<br>");
+        document.write("Total Owing:"+" "+totalAccountBalance);
+    }
+    else if(account_Balance==0){
+        document.write("Full Name:"+ " "+ fullName);
+        document.write("<br>");
+        document.write("Address:"+" "+ address);
+        document.write("<br>");
+        document.write("City:"+" "+city);
+        document.write("<br>");
+        document.write("Province:"+" "+province);
+        document.write("<br>");
+        document.write("Account Balance:"+" "+accountBalance);
+        document.write("<br>");document.write("Thank you for the Payment!!!");
+        
+    }
 }
